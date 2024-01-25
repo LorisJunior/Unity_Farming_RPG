66f3613f3d306f3d4af3a363afc215fa5154d7b6
Lourival Menezes <em-lourival@outlook.com>
2024-01-25 10:28:14 -0300

    Player basic configurations done

---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,48 @@
+using System;
+
+public delegate void MovementDelegate(
+    float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLisftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight
+);
+
+public static class EventHandler
+{
+    // Movement Event
+
+    public static event MovementDelegate MovementEvent;
+
+    // Movement Event Call For Publishers
+
+    public static void CallMovementEvent(
+        float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+        ToolEffect toolEffect,
+        bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+        bool isLiftingToolRight, bool isLiftingToolLeft, bool isLisftingToolUp, bool isLiftingToolDown,
+        bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+        bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+        bool idleUp, bool idleDown, bool idleLeft, bool idleRight)
+    {
+        if (MovementEvent != null)
+        {
+            MovementEvent(
+                inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLisftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                idleUp, idleDown, idleLeft, idleRight
+            );
+        }
+    }
+
+    internal static void CallMovementEvent()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Misc/SingletonMonobehaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public T Instance 
+    { 
+        get
+        {
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
